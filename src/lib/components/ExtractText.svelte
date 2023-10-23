f22fa8924524cd0ca4db9a2d69c675229f0c8872
Henok Ayenew <henacodes@gmail.com>
2023-10-23 14:31:29 +0400

    working on the camera capture

--- src/lib/components/ExtractText.svelte
+++ src/lib/components/ExtractText.svelte
@@ -1,7 +1,106 @@
 <script>
-    
+
+    import { onMount, onDestroy } from "svelte"
+    let videoEl;
+    let imageBase64 = "";
+    let cameraMode = "user";
+    import 'iconify-icon'
+
+
+
+    const startVid = async () => {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({
+        audio: false,
+        video: {
+          width: 1000,
+          height: 1000,
+          facingMode: { exact: cameraMode },
+        },
+      });
+      console.log(stream);
+      videoEl.srcObject = stream;
+      videoEl.style.display = "block";
+      imageBase64 = "";
+    } catch (error) {
+      console.error("Error accessing camera:", error);
+    }
+  };
+  const stopVideo = () =>{
+   if (videoEl?.srcObject){
+    const stream = videoEl.srcObject;
+    const tracks = stream.getTracks();
+    tracks.forEach(track => track.stop());
+    videoEl.srcObject = null;
+    videoEl.style.display = "none";
+   }
+  }
+  async function performOCR(imageData) {
+    const worker = await createWorker("eng");
+    (async () => {
+      const {
+        data: { text },
+      } = await worker.recognize(imageData);
+      console.log(text);
+      alert(text);
+      await worker.terminate();
+    })();
+  }
+  async function captureImage() {
+    const canvas = document.createElement("canvas");
+    const context = canvas.getContext("2d");
+    canvas.width = videoEl.videoWidth;
+    canvas.height = videoEl.videoHeight;
+    context.drawImage(videoEl, 0, 0, canvas.width, canvas.height);
+    const dataURL = canvas.toDataURL("image/jpeg");
+    imageBase64 = dataURL;
+    videoEl.style.display = "none";
+    console.log(imageBase64)
+    stopVideo()
+    console.log("after", imageBase64)
+  }
+
+
+ 
+
+  onMount(() =>{
+    startVid()
+  })
+
+  onDestroy(() =>{
+    stopVideo()
+  })
 </script>
 
-<h1>Modal compoent</h1>
+<div class="card p-4 variant-soft-surface w-[80vw]">
+    <div class="flex flex-col items-center" >
+        <!-- svelte-ignore a11y-media-has-caption -->
+        
+       {#if imageBase64}
+       <img class="w-[60%] rounded-md" src={imageBase64} alt="" />
+       {:else}
+       <video bind:this={videoEl} autoplay class=" w-[60%] rounded-md "  />
 
-<h1>Hhhh</h1>
+       {/if}
+        <div class="flex flex-col items-center ">   
+           {#if !imageBase64}
+              <button on:click={captureImage} type="button" class="btn-icon variant-filled">
+                <iconify-icon icon="mdi:camera" class="flex items-center" ></iconify-icon>
+            </button>
+           {/if}
+          <div>
+          {#if imageBase64}
+            <button on:click={() => performOCR(imageBase64)} type="button" class="btn btn-sm variant-filled">Extract</button>
+          {:else}
+          <button
+                on:click={() => {
+            cameraMode = "environment";
+            startVid();
+            }}>Rear camera</button
+            >
+          {/if}
+          
+           </div>
+        </div>
+    </div>
+</div>
--- src/lib/components/InputMessage.svelte
+++ src/lib/components/InputMessage.svelte
@@ -1,7 +1,8 @@
 <script>
 	import { getModalStore } from '@skeletonlabs/skeleton';
 	const modalStore = getModalStore();
-	
+	import 'iconify-icon'
+
 	
 
 	const modal = {
@@ -14,7 +15,8 @@
 
 
 <div class="input-group input-group-divider grid-cols-[auto_1fr_auto] rounded-container-token mt-3 ">
-	<button on:click={() => modalStore.trigger(modal)}  class="input-group-shim"> <iconify-icon icon="mdi:camera"></iconify-icon>
+	<button on:click={() => modalStore.trigger(modal)}  class="input-group-shim"> 
+		<iconify-icon icon="mdi:camera"></iconify-icon>
 	</button>
 	<input class="input focus:outline-none border-none pl-3 py-2  " type="text" placeholder="Write your prompt here" />
 	<button class="variant-filled-primary">Send</button>
